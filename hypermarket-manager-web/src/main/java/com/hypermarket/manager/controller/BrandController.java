f78c15896e1f597cf43c240b4e06442aeb078a2a
ytlxy <a739771409@gmail.com>
2020-10-11 15:01:37 +0800

    new

--- hypermarket-manager-web/src/main/java/com/hypermarket/manager/controller/BrandController.java
+++ hypermarket-manager-web/src/main/java/com/hypermarket/manager/controller/BrandController.java
@@ -2,9 +2,12 @@ package com.hypermarket.manager.controller;
 
 import com.alibaba.dubbo.config.annotation.Reference;
 import com.hypermarket.entity.PageResult;
+import com.hypermarket.entity.Result;
 import com.hypermarket.pojo.TbBrand;
 import com.hypermarket.sellergoods.service.BrandService;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -23,5 +26,16 @@ public class BrandController {
     @RequestMapping("/findPage")
     public PageResult findPage(int page,int size){
         return brandService.findPage(page, size);
+    }
+
+    @RequestMapping("/add")
+    public Result add(@RequestBody TbBrand brand){
+        try {
+            brandService.add(brand);
+            return new Result(true,"增加成功");
+        }catch (Exception e){
+            e.printStackTrace();
+            return new Result(false,"增加失败");
+        }
     }
 }
--- hypermarket-sellergoods/hypermarket-sellergoods-interface/src/main/java/com/hypermarket/sellergoods/service/BrandService.java
+++ hypermarket-sellergoods/hypermarket-sellergoods-interface/src/main/java/com/hypermarket/sellergoods/service/BrandService.java
@@ -8,5 +8,5 @@ import java.util.List;
 public interface BrandService {
     public List<TbBrand> findAll();
     public PageResult findPage(int pageNum,int pageSize);
-
+    public void add(TbBrand brand);
 }
