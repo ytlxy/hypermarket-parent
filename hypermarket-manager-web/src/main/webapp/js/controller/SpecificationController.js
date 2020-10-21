3e25e8db96f4bb556cf3e433b15d13c051f73beb
ytlxy <a739771409@gmail.com>
2020-10-21 22:52:37 +0800

    new

--- hypermarket-manager-web/src/main/webapp/js/controller/SpecificationController.js
+++ hypermarket-manager-web/src/main/webapp/js/controller/SpecificationController.js
@@ -1,89 +1,43 @@
-//控制层
-app.controller('SpecificationControllrt',function ($scope,$controller,SpecificationService) {
-    $controller('baseController',{$scope:$scope});//继承
-
-    //读取列表数据绑定到表单中
-    $scope.findAll=function () {
-        SpecificationService.findAll().success(
-            function (response) {
-                $scope.list=response;
-            }
-        );
+//服务层
+app.service('SpecificationService',function ($http) {
+    //读取列表数据绑定到列表
+    this.findAll=function () {
+        return $http.get('../Specification/findAll.do');
     }
 
     //分页
-    $scope.findPage=function (page,rows) {
-        SpecificationService.findAll(page,rows).success(
-          function (response) {
-            $scope.list=response.rows;
-            $scope.paginationConf.totalItems=response.total;//更新总记录数
-          }
-        );
+    this.findPage=function (page,rows) {
+        return $http.get('../Specification/findPage.do?page='+page+'&rows='+rows);
     }
 
     //查询实体
-    $scope.findOne=function (id) {
-        SpecificationService.findOne(id).success(
-           function (response) {
-                $scope.entity=response;
-           }
-        );
+    this.findOne=function (id) {
+        return $http.get('../Specification/findOne.do?id='+id);
     }
 
-    //保存
-    $scope.save=function () {
-        var serviceObject; //服务层对象
-        if ($scope.entity.specification.id!=null){
-            serviceObjec=SpecificationService.update($scope.entity); //修改
-        }else {
-            serviceObject=SpecificationService.add($scope.entity); //增加
-        }
-        serviceObject.success(
-          function (response) {
-            if (response.success){
-                //重新查询
-                $scope.reloadList(); //重新加载
-            }else {
-                alert(response.message);
-            }
-          }
-        );
+    //增加
+    this.add=function (entity) {
+        return $http.post('../Specification/add.do',entity);
     }
 
-    //批量删除
-    $scope.detele=function () {
-        //获取选中的复选框
-        SpecificationService.detele($scope.selectIds).success(
-            function (response) {
-                if (response.success){
-                    $scope.reloadList(); //刷新
-                    $scope.selectIds=[];
-                }
-            }
-        );
+    //修改
+    this.update=function (entity) {
+        return $http.post('../Specification/update.do',entity);
     }
 
-    $scope.searchEntity={}; //定义搜索对象
+    //删除
+    this.detect=function (ids) {
+        return $http.post('../Specification/detele.do?ids=',ids);
+    }
 
     //搜索
-    $scope.search=function (page,rows) {
-        SpecificationService.search(page,rows,$scope.searchEntity).success(
-            function (response) {
-                $scope.list=response.rows;
-                $scope.paginationConf.totalItems=response.total;//更新总记录数
-            }
-        );
+    this.search=function (page,rows,searchEntity) {
+        return $http.post('../Specification/search.do?page='+page+'rows'+rows,searchEntity);
     }
 
-    $scope.entity={selectOptionList:[]};
-
-    //增加规格选项行
-    $scope.addTableRow=function () {
-        $scope.entity.selectOptionList.push({});
+    //下拉列表
+    this.selectOptionList=function () {
+        return $http.get('../Specification/selectOptionList.do');
     }
 
-    //删除规格选项行
-    $scope.deteleTableRow=function () {
-        $scope.entity.selectOptionList.splice(index,1);
-    }
 });
