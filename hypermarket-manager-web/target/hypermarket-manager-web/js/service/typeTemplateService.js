11651010195c6e4f7c4ae0457e7e5d50011675dc
ytlxy <a739771409@gmail.com>
2020-11-03 10:44:22 +0800

    new

--- hypermarket-manager-web/target/hypermarket-manager-web/js/service/typeTemplateService.js
+++ hypermarket-manager-web/target/hypermarket-manager-web/js/service/typeTemplateService.js
@@ -1,11 +1,11 @@
 //服务层
 app.service('typeTemplateService',function($http){
-	    	
+
 	//读取列表数据绑定到表单中
 	this.findAll=function(){
-		return $http.get('../typeTemplate/findAll.do');		
+		return $http.get('../typeTemplate/findAll.do');
 	}
-	//分页 
+	//分页
 	this.findPage=function(page,rows){
 		return $http.get('../typeTemplate/findPage.do?page='+page+'&rows='+rows);
 	}
@@ -13,20 +13,20 @@ app.service('typeTemplateService',function($http){
 	this.findOne=function(id){
 		return $http.get('../typeTemplate/findOne.do?id='+id);
 	}
-	//增加 
+	//增加
 	this.add=function(entity){
 		return  $http.post('../typeTemplate/add.do',entity );
 	}
-	//修改 
+	//修改
 	this.update=function(entity){
 		return  $http.post('../typeTemplate/update.do',entity );
 	}
 	//删除
-	this.dele=function(ids){
+	this.deletes=function(ids){
 		return $http.post('../typeTemplate/delete.do?ids=',ids);
 	}
 	//搜索
 	this.search=function(page,rows,searchEntity){
 		return $http.post('../typeTemplate/search.do?page='+page+"&rows="+rows, searchEntity);
-	}    	
+	}
 });
